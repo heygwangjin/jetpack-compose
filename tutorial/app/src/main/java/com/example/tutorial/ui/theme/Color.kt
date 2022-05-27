dac53e77be52d27d926f5b98e370fce20ba02148
heygwangjin <heygwangjin@gmail.com>
2022-05-28 06:58:59 +0900

    jetpack compose basics

---
+++ tutorial/app/src/main/java/com/example/tutorial/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.tutorial.ui.theme
+import androidx.compose.ui.graphics.Color
+
+
+val md_theme_light_primary = Color(0xFFE99154)
+val md_theme_light_onPrimary = Color(0xFFffffff)
+val md_theme_light_secondary = Color(0xFF3660E0)
+val md_theme_light_onSecondary = Color(0xFFffffff)
+val md_theme_light_error = Color(0xFFba1b1b)
+val md_theme_light_onError = Color(0xFFffffff)
+val md_theme_light_background = Color(0xFFfcfcfc)
+val md_theme_light_onBackground = Color(0xFF201a17)
+val md_theme_light_surface = Color(0xFFfcfcfc)
+val md_theme_light_onSurface = Color(0xFF201a17)
+
+val md_theme_dark_primary = Color(0xFFffb685)
+val md_theme_dark_onPrimary = Color(0xFF522300)
+val md_theme_dark_secondary = Color(0xFFb7c4ff)
+val md_theme_dark_onSecondary = Color(0xFF00238a)
+val md_theme_dark_error = Color(0xFFffb4a9)
+val md_theme_dark_onError = Color(0xFF680003)
+val md_theme_dark_background = Color(0xFF201a17)
+val md_theme_dark_onBackground = Color(0xFFece0da)
+val md_theme_dark_surface = Color(0xFF201a17)
+val md_theme_dark_onSurface = Color(0xFFece0da)
+
+val seed = Color(0xFFf38f48)
+val error = Color(0xFFba1b1b)
